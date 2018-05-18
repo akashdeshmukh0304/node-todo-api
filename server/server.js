39ef2da6905432ddc50f2ab62c29bec128f2c10c
akash <akash@localhost.localdomain>
2018-05-18 08:39:32 +0530

    Add GET Todos id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectId} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -39,6 +41,25 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectId.isValid(id)) {
+		res.status(404).send({
+			error: 'Invalid object Id'
+		});
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send({
+				error: 'No data found'
+			});
+		} else {
+			res.status(200).send({todo});
+		}
+	}, (e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
